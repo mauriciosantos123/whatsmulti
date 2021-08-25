a438abf6e8accd949808ec2d76dae77fb45530fe
Mauricio Vieira <mauriciusantos123@hotmail.com>
2021-08-25 17:52:45 -0300

    25/08/21

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -1,18 +1,29 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext }  from 'react';
 import clsx from "clsx";
 
+import PropTypes from 'prop-types';
+
 import {
-	makeStyles,
-	Drawer,
-	AppBar,
-	Toolbar,
-	List,
-	Typography,
-	Divider,
+  AppBar,
+  CssBaseline,
+  Drawer,
+  Divider,
+  Hidden,
+  IconButton,
+  InboxIcon,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  MailIcon,
 	MenuItem,
-	IconButton,
 	Menu,
 } from "@material-ui/core";
+
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
 
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
@@ -28,78 +39,65 @@ import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		display: "flex",
-		height: "100vh",
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    height: "100vh",
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  title: {
+		flexGrow: 1,
 	},
-
-	toolbar: {
-		paddingRight: 24, // keep right padding when drawer closed
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  appBarSpacer: {
+		minHeight: "48px",
 	},
-	toolbarIcon: {
+  content: {
+		flex: 1,
+		overflow: "auto",
+	},
+  menuButton: {
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  
+  menuButtonHidden: {
+		display: "none",
+	},
+  container: {
+		paddingTop: theme.spacing(4),
+		paddingBottom: theme.spacing(4),
+	},
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  
+  	toolbarIcon: {
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "flex-end",
 		padding: "0 8px",
 		minHeight: "48px",
 	},
-	appBar: {
-		zIndex: theme.zIndex.drawer + 1,
-		transition: theme.transitions.create(["width", "margin"], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen,
-		}),
-	},
-	appBarShift: {
-		marginLeft: drawerWidth,
-		width: `calc(100% - ${drawerWidth}px)`,
-		transition: theme.transitions.create(["width", "margin"], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.enteringScreen,
-		}),
-	},
-	menuButton: {
-		marginRight: 36,
-	},
-	menuButtonHidden: {
-		display: "none",
-	},
-	title: {
-		flexGrow: 1,
-	},
-	drawerPaper: {
-		position: "relative",
-		whiteSpace: "nowrap",
-		width: drawerWidth,
-		transition: theme.transitions.create("width", {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.enteringScreen,
-		}),
-	},
-	drawerPaperClose: {
-		overflowX: "hidden",
-		transition: theme.transitions.create("width", {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen,
-		}),
-		width: theme.spacing(7),
-		[theme.breakpoints.up("sm")]: {
-			width: theme.spacing(9),
-		},
-	},
-	appBarSpacer: {
-		minHeight: "48px",
-	},
-	content: {
-		flex: 1,
-		overflow: "auto",
-	},
-	container: {
-		paddingTop: theme.spacing(4),
-		paddingBottom: theme.spacing(4),
-	},
-	paper: {
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(),
+  },
+  paper: {
 		padding: theme.spacing(2),
 		display: "flex",
 		overflow: "auto",
@@ -107,21 +105,23 @@ const useStyles = makeStyles(theme => ({
 	},
 }));
 
-const LoggedInLayout = ({ children }) => {
-	const classes = useStyles();
-	const [userModalOpen, setUserModalOpen] = useState(false);
-	const [anchorEl, setAnchorEl] = useState(null);
+function ResponsiveDrawer({ children }) {
+  const { window } = { children };
+  const [userModalOpen, setUserModalOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 	const [menuOpen, setMenuOpen] = useState(false);
 	const { handleLogout, loading } = useContext(AuthContext);
-	const [drawerOpen, setDrawerOpen] = useLocalStorage("drawerOpen", true);
-	const { user } = useContext(AuthContext);
+  const [drawerOpen, setDrawerOpen] = useLocalStorage("drawerOpen", false);
+  const { user } = useContext(AuthContext);
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-	const handleMenu = event => {
+  const handleMenu = event => {
 		setAnchorEl(event.currentTarget);
 		setMenuOpen(true);
 	};
-
-	const handleCloseMenu = () => {
+  const handleCloseMenu = () => {
 		setAnchorEl(null);
 		setMenuOpen(false);
 	};
@@ -136,69 +136,63 @@ const LoggedInLayout = ({ children }) => {
 		handleLogout();
 	};
 
-	if (loading) {
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  if (loading) {
 		return <BackdropLoading />;
 	}
 
-	return (
-		<div className={classes.root}>
-			<Drawer
-				variant="permanent"
-				classes={{
-					paper: clsx(
-						classes.drawerPaper,
-						!drawerOpen && classes.drawerPaperClose
-					),
-				}}
-				open={drawerOpen}
-			>
-				<div className={classes.toolbarIcon}>
-					<IconButton onClick={() => setDrawerOpen(!drawerOpen)}>
-						<ChevronLeftIcon />
-					</IconButton>
-				</div>
-				<Divider />
-				<List>
-					<MainListItems />
-				</List>
-				<Divider />
-			</Drawer>
-			<UserModal
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+
+      <List>
+	  <MainListItems />
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      
+      <UserModal
 				open={userModalOpen}
 				onClose={() => setUserModalOpen(false)}
 				userId={user?.id}
 			/>
-			<AppBar
-				position="absolute"
-				className={clsx(classes.appBar, drawerOpen && classes.appBarShift)}
-				color={process.env.NODE_ENV === "development" ? "inherit" : "primary"}
-			>
-				<Toolbar variant="dense" className={classes.toolbar}>
-					<IconButton
-						edge="start"
-						color="inherit"
-						aria-label="open drawer"
-						onClick={() => setDrawerOpen(!drawerOpen)}
-						className={clsx(
-							classes.menuButton,
-							drawerOpen && classes.menuButtonHidden
-						)}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography
+      
+      <AppBar position="fixed" className={classes.appBar} 
+      color={process.env.NODE_ENV === "development" ? "inherit" : "primary"}>
+        <Toolbar variant="dense" className={classes.toolbar}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+          
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+		  <Typography
 						component="h1"
 						variant="h6"
 						color="inherit"
 						noWrap
 						className={classes.title}
+						
 					>
 						WhatsMulti - FaleTotal
 					</Typography>
-					{user.id && <NotificationsPopOver />}
-
-					<div>
-						<IconButton
+					
+						{user.id && <NotificationsPopOver />}
+            <div>
+            <IconButton
 							aria-label="account of current user"
 							aria-controls="menu-appbar"
 							aria-haspopup="true"
@@ -207,7 +201,8 @@ const LoggedInLayout = ({ children }) => {
 						>
 							<AccountCircle />
 						</IconButton>
-						<Menu
+
+            <Menu
 							id="menu-appbar"
 							anchorEl={anchorEl}
 							getContentAnchorEl={null}
@@ -229,16 +224,55 @@ const LoggedInLayout = ({ children }) => {
 								{i18n.t("mainDrawer.appBar.user.logout")}
 							</MenuItem>
 						</Menu>
-					</div>
-				</Toolbar>
-			</AppBar>
-			<main className={classes.content}>
+            </div>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+	  <main className={classes.content}>
 				<div className={classes.appBarSpacer} />
 
 				{children ? children : null}
 			</main>
-		</div>
-	);
+    </div>
+  );
+}
+
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
-export default LoggedInLayout;
+export default ResponsiveDrawer;
